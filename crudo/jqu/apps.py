ee420891ebce76681f74f2fec3e3a98a1395fdd7
jeybrindh <122431515+jeybrindh@users.noreply.github.com>
2023-01-11 12:33:19 +0530

    Add files via upload

---
+++ crudo/jqu/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class JquConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'jqu'
---
+++ crudo/jqu/urls.py
@@ -0,0 +1,14 @@
+#from django.contrib import admin  
+from django.urls import path  
+from jqu import views  
+#from django.conf.urls import url
+ 
+urlpatterns = [  
+    #path('admin/', admin.site.urls),  
+    path('',views.index, name='index'),
+    path('create', views.create, name='create'),
+    path('read', views.read, name='read'),
+    path('edit/<id>', views.edit, name='edit'),
+    path('edit/update/<id>', views.update, name='update'),
+    path('delete/<id>', views.delete, name='delete'),
+] 
